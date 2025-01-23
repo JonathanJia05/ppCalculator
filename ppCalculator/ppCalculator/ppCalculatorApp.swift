6148323ec1655ede9ea4699a78634f6aeaf16d16
Jonathan Jia <jonathanj18456@gmail.com>
2025-01-22 23:59:14 -0500

    recreated xcode project

---
+++ ppCalculator/ppCalculator/ppCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ppCalculatorApp.swift
+//  ppCalculator
+//
+//  Created by Jonathan Jia on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ppCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
